cb873f3f9b8ee9e93f43ebb949e587e86c40caff
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-06-03 17:19:35 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,81 +1,31 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "Untitled1.ipynb",
-      "provenance": [],
-      "authorship_tag": "ABX9TyPO69/FR8seOyiuiSR7gtSH"
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "code",
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "dVKn1-TrNMlV",
-        "outputId": "b76d7fe1-46ed-495f-903d-674fa7570fa5"
-      },
-      "source": [
-        "# Написать цикл для выведения на экран каждой буквы своего ФИО\n",
-        "\n",
-        "name = input('Введите своё ФИО >>> ').title()\n",
-        "for el in name:\n",
-        "  print(el)"
-      ],
-      "execution_count": 3,
-      "outputs": [
-        {
-          "output_type": "stream",
-          "text": [
-            "Введите своё ФИО >>> корнилова анна павловна\n",
-            "К\n",
-            "о\n",
-            "р\n",
-            "н\n",
-            "и\n",
-            "л\n",
-            "о\n",
-            "в\n",
-            "а\n",
-            " \n",
-            "А\n",
-            "н\n",
-            "н\n",
-            "а\n",
-            " \n",
-            "П\n",
-            "а\n",
-            "в\n",
-            "л\n",
-            "о\n",
-            "в\n",
-            "н\n",
-            "а\n"
-          ],
-          "name": "stdout"
-        }
-      ]
-    },
-    {
-      "cell_type": "code",
-      "metadata": {
-        "id": "msgmQKtgNzXZ"
-      },
-      "source": [
-        ""
-      ],
-      "execution_count": null,
-      "outputs": []
-    }
-  ]
-}
+# Написать цикл для выведения на экран каждой буквы своего ФИО
+
+name = input('Введите своё ФИО >>> ').title()
+for el in name:
+  print(el)
+______________
+Введите своё ФИО >>> корнилова анна павловна
+К
+о
+р
+н
+и
+л
+о
+в
+а
+ 
+А
+н
+н
+а
+ 
+П
+а
+в
+л
+о
+в
+н
+а
+     
